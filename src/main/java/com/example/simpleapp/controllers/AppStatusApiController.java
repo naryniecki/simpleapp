6bba4cd8dd982c6b0ec8afe0e83c43486c40d081
Pawel Naryniecki <pawel.naryniecki@oracle.com>
2019-10-18 17:29:21 +0100

    All

---
+++ src/main/java/com/example/simpleapp/controllers/AppStatusApiController.java
@@ -0,0 +1,30 @@
+package com.example.simpleapp.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppStatusApiController {
+    @PostMapping("/install")
+    public ResponseEntity install() {
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/uninstall")
+    public ResponseEntity uninstall() {
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("configure")
+    public ResponseEntity configure() {
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity status() {
+        return ResponseEntity.ok("OK");
+    }
+}
+
