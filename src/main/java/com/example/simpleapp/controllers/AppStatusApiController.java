902e8962cee60e6d27a77fc788f8a963ff261265
Pawel Naryniecki <pawel.naryniecki@oracle.com>
2019-11-04 15:34:45 +0100

    test2

--- src/main/java/com/example/simpleapp/controllers/AppStatusApiController.java
+++ src/main/java/com/example/simpleapp/controllers/AppStatusApiController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,15 +23,16 @@ public class AppStatusApiController {
     private List<String> secrets = new ArrayList<>();
 
     @PostMapping("/install")
-    public ResponseEntity install(@RequestParam("installId") String installId,
+    public ModelAndView install(@RequestParam("installId") String installId,
                                   @RequestParam("userId") String userId,
                                   @RequestParam("userName") String userName,
                                   @RequestParam("siteId") String siteId,
                                   @RequestParam("siteName") String siteName,
-                                  @RequestParam("appId") String appId
+                                  @RequestParam("appId") String appId,
+                                  @RequestParam("callback") String callback
     ) {
         installationsService.installApplication(installId, userId, userName, siteId, siteName, appId);
-        return ResponseEntity.ok(installId);
+        return new ModelAndView("redirect:" + callback);
     }
 
     @PostMapping("/uninstall")
