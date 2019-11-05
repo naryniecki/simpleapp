fb481b14d8cfccb356cb5dd893d6ae0031c66606
Pawel Naryniecki <pawel.naryniecki@gmail.com>
2019-11-05 07:47:21 +0100

    test3

--- src/main/java/com/example/simpleapp/SecurityConfig.java
+++ src/main/java/com/example/simpleapp/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.example.simpleapp;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import static com.example.simpleapp.Constants.ELOQUA_AUTHORIZATION_CODE_ENDPOINT;
 import static com.example.simpleapp.Constants.ELOQUA_CONFIGURE_ENDPOINT;
@@ -14,22 +14,22 @@ import static com.example.simpleapp.Constants.ELOQUA_UNINSTALL_ENDPOINT;
 
 //@Configuration
 //@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/login*", ELOQUA_AUTHORIZATION_CODE_ENDPOINT)
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2Login();
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(ELOQUA_CONFIGURE_ENDPOINT, ELOQUA_INSTALL_ENDPOINT, ELOQUA_UNINSTALL_ENDPOINT, ELOQUA_STATUS_ENDPOINT, "/all");
-    }
-}
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/login*", ELOQUA_AUTHORIZATION_CODE_ENDPOINT)
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .oauth2Login();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web.ignoring().antMatchers(ELOQUA_CONFIGURE_ENDPOINT, ELOQUA_INSTALL_ENDPOINT, ELOQUA_UNINSTALL_ENDPOINT, ELOQUA_STATUS_ENDPOINT, "/all");
+//    }
+//}
--- src/main/java/com/example/simpleapp/SimpleappApplication.java
+++ src/main/java/com/example/simpleapp/SimpleappApplication.java
@@ -5,10 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+//import org.springframework.security.oauth2.client.OAuth2ClientContext;
+//import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+//import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+//import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 
 import java.util.Arrays;
 
