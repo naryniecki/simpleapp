14c6e613f3f024d2448c400e690c1e07c93983ec
Pawel Naryniecki <pawel.naryniecki@oracle.com>
2019-11-05 12:00:19 +0100

    test3

--- src/main/java/com/example/simpleapp/SecurityConfig.java
+++ src/main/java/com/example/simpleapp/SecurityConfig.java
@@ -1,35 +1,35 @@
-package com.example.simpleapp;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import static com.example.simpleapp.Constants.ELOQUA_AUTHORIZATION_CODE_ENDPOINT;
-import static com.example.simpleapp.Constants.ELOQUA_CONFIGURE_ENDPOINT;
-import static com.example.simpleapp.Constants.ELOQUA_INSTALL_ENDPOINT;
-import static com.example.simpleapp.Constants.ELOQUA_STATUS_ENDPOINT;
-import static com.example.simpleapp.Constants.ELOQUA_UNINSTALL_ENDPOINT;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/login*")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2Login();
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(ELOQUA_CONFIGURE_ENDPOINT, ELOQUA_INSTALL_ENDPOINT, ELOQUA_UNINSTALL_ENDPOINT, ELOQUA_STATUS_ENDPOINT, ELOQUA_AUTHORIZATION_CODE_ENDPOINT, "/all");
-    }
-}
+//package com.example.simpleapp;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//import static com.example.simpleapp.Constants.ELOQUA_AUTHORIZATION_CODE_ENDPOINT;
+//import static com.example.simpleapp.Constants.ELOQUA_CONFIGURE_ENDPOINT;
+//import static com.example.simpleapp.Constants.ELOQUA_INSTALL_ENDPOINT;
+//import static com.example.simpleapp.Constants.ELOQUA_STATUS_ENDPOINT;
+//import static com.example.simpleapp.Constants.ELOQUA_UNINSTALL_ENDPOINT;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/login*")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .oauth2Login();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web.ignoring().antMatchers(ELOQUA_CONFIGURE_ENDPOINT, ELOQUA_INSTALL_ENDPOINT, ELOQUA_UNINSTALL_ENDPOINT, ELOQUA_STATUS_ENDPOINT, ELOQUA_AUTHORIZATION_CODE_ENDPOINT, "/all", "/code");
+//    }
+//}
